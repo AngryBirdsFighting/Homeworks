9602b3576451b1ef32ccbffc46e90e9cc40cb696
JoeZhiPeng <614231663@qq.com>
2019-03-30 20:08:28 +0800

    contain

--- Homework-wang/src/views/layout/Content.jsx
+++ Homework-wang/src/views/layout/Content.jsx
@@ -6,14 +6,23 @@
  * @Description:  
  */
 import React,{Component} from "react"
+import window from "../../assets/os icons/windows.png"
 
 class Home extends Component{
+    state={
+        height:"",
+    }
     componentWillMount() {
         console.log(this.props)
-     }
+    }
+    componentDidMount(){
+        this.setState({
+            height:(document.documentElement.clientHeight - 70)+ "px"
+        })
+    }
     render(){
         return(
-            <div className="content-container">
+            <div style={{height:this.state.height}} className="content-container">
                 <div>
                     <ul className="statistics">
                         <li>
@@ -61,6 +70,233 @@ class Home extends Component{
                             <i className="icon-th-list"></i>
                         </div>
                         <span className="clear"></span>
+                    </div>
+                    <div >
+                    <div className="content">
+                            <div className="fl">
+                                <img src={window} className="window"></img>
+                            </div>
+                            <div className="fl content-right">
+                                <ul>
+                                    <li>
+                                        <i className="icon-desktop"></i>
+                                        <span className="sp1">bjstdmngbgr08.thoughthomeworks.com</span>
+                                        <span className="sp2">building</span>
+                                    </li>
+                                    <li>
+                                        <i className="icon-info"></i>
+                                        <span className="sp3">192.168.1.243</span>
+                                    </li>
+                                    <li>
+                                        <i className="icon-folder"></i>
+                                        <span>/var/lib/cruise-agent</span>
+                                    </li>
+                                </ul>
+                                <ul className="con-bottom">
+                                    <li>
+                                        <i className="icon-plus"></i>
+                                        <span>
+                                            Fixfox<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Safari<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Ubuntu<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Chrome<i className="icon-trash"></i>
+                                        </span>
+                                    </li>
+                                    <li className="fr">
+                                        <span>
+                                            <i className="icon-deny"></i>Deny
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <span className="clear"></span>
+                        </div>
+                        <div className="content">
+                            <div className="fl">
+                                <img src={window} className="window"></img>
+                            </div>
+                            <div className="fl content-right">
+                                <ul>
+                                    <li>
+                                        <i className="icon-desktop"></i>
+                                        <span className="sp1">bjstdmngbgr08.thoughthomeworks.com</span>
+                                        <span className="sp2">building</span>
+                                    </li>
+                                    <li>
+                                        <i className="icon-info"></i>
+                                        <span className="sp3">192.168.1.243</span>
+                                    </li>
+                                    <li>
+                                        <i className="icon-folder"></i>
+                                        <span>/var/lib/cruise-agent</span>
+                                    </li>
+                                </ul>
+                                <ul className="con-bottom">
+                                    <li>
+                                        <i className="icon-plus"></i>
+                                        <span>
+                                            Fixfox<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Safari<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Ubuntu<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Chrome<i className="icon-trash"></i>
+                                        </span>
+                                    </li>
+                                    <li className="fr">
+                                        <span>
+                                            <i className="icon-deny"></i>Deny
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <span className="clear"></span>
+                        </div>
+                        <div className="content">
+                            <div className="fl">
+                                <img src={window} className="window"></img>
+                            </div>
+                            <div className="fl content-right">
+                                <ul>
+                                    <li>
+                                        <i className="icon-desktop"></i>
+                                        <span className="sp1">bjstdmngbgr08.thoughthomeworks.com</span>
+                                        <span className="sp2">building</span>
+                                    </li>
+                                    <li>
+                                        <i className="icon-info"></i>
+                                        <span className="sp3">192.168.1.243</span>
+                                    </li>
+                                    <li>
+                                        <i className="icon-folder"></i>
+                                        <span>/var/lib/cruise-agent</span>
+                                    </li>
+                                </ul>
+                                <ul className="con-bottom">
+                                    <li>
+                                        <i className="icon-plus"></i>
+                                        <span>
+                                            Fixfox<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Safari<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Ubuntu<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Chrome<i className="icon-trash"></i>
+                                        </span>
+                                    </li>
+                                    <li className="fr">
+                                        <span>
+                                            <i className="icon-deny"></i>Deny
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <span className="clear"></span>
+                        </div>
+                        <div className="content">
+                            <div className="fl">
+                                <img src={window} className="window"></img>
+                            </div>
+                            <div className="fl content-right">
+                                <ul>
+                                    <li>
+                                        <i className="icon-desktop"></i>
+                                        <span className="sp1">bjstdmngbgr08.thoughthomeworks.com</span>
+                                        <span className="sp2">building</span>
+                                    </li>
+                                    <li>
+                                        <i className="icon-info"></i>
+                                        <span className="sp3">192.168.1.243</span>
+                                    </li>
+                                    <li>
+                                        <i className="icon-folder"></i>
+                                        <span>/var/lib/cruise-agent</span>
+                                    </li>
+                                </ul>
+                                <ul className="con-bottom">
+                                    <li>
+                                        <i className="icon-plus"></i>
+                                        <span>
+                                            Fixfox<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Safari<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Ubuntu<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Chrome<i className="icon-trash"></i>
+                                        </span>
+                                    </li>
+                                    <li className="fr">
+                                        <span>
+                                            <i className="icon-deny"></i>Deny
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <span className="clear"></span>
+                        </div>
+                        <div className="content">
+                            <div className="fl">
+                                <img src={window} className="window"></img>
+                            </div>
+                            <div className="fl content-right">
+                                <ul>
+                                    <li>
+                                        <i className="icon-desktop"></i>
+                                        <span className="sp1">bjstdmngbgr08.thoughthomeworks.com</span>
+                                        <span className="sp2">building</span>
+                                    </li>
+                                    <li>
+                                        <i className="icon-info"></i>
+                                        <span className="sp3">192.168.1.243</span>
+                                    </li>
+                                    <li>
+                                        <i className="icon-folder"></i>
+                                        <span>/var/lib/cruise-agent</span>
+                                    </li>
+                                </ul>
+                                <ul className="con-bottom">
+                                    <li>
+                                        <i className="icon-plus"></i>
+                                        <span>
+                                            Fixfox<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Safari<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Ubuntu<i className="icon-trash"></i>
+                                        </span>
+                                        <span>
+                                            Chrome<i className="icon-trash"></i>
+                                        </span>
+                                    </li>
+                                    <li className="fr">
+                                        <span>
+                                            <i className="icon-deny"></i>Deny
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <span className="clear"></span>
+                        </div>
                     </div>
                 </div>
             </div>
