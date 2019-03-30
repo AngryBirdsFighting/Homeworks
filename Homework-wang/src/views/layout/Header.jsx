097d782809daa301be1af115732d7df4acbdeea1
JoeZhiPeng <614231663@qq.com>
2019-03-30 16:42:12 +0800

    header

--- Homework-wang/src/views/layout/Header.jsx
+++ Homework-wang/src/views/layout/Header.jsx
@@ -18,8 +18,19 @@ class Home extends Component{
                <header className="header-container">
                     <div className="header">
                         <i className="fl icon icon-navicon"></i>
+                        <i className="fr icon icon-angle-down"></i>
                         <img  className="fr avatar"  src={avatar} alt="头像" height="40px" />
                         <div className="clear"></div>
+                        <ul>
+                            <li>
+                                <i className="icon-id-card"></i>
+                                <span>Profile</span>
+                            </li>
+                            <li>
+                                <i className="icon-sign-in"></i>
+                                <span>Sign Out</span>
+                            </li>
+                        </ul>
                     </div>
                </header>
             </div>
